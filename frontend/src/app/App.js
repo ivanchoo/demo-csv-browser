a5698a2cd7f7915602b7f9e0d939b6ef50f9fc0b
Ivan Choo <ivanchoo.private@gmail.com>
2017-09-22 13:47:09 +0800

    Add Bootstrap4

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 class App extends React.Component {
 
   render() {
-    return <div>Help!</div>;
+    return <div>Hell world!</div>;
   }
 }
 
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   entry: {
     index: entrypoint([
       'babel-polyfill',
+      'bootstrap-loader',
       r(SRC_DIR, 'index.js'),
     ]),
   },
@@ -60,9 +61,23 @@ module.exports = {
        }
     }),
     new webpack.ProvidePlugin({
-      $               : 'jquery',
-      jQuery          : 'jquery',
-      'window.jQuery' : 'jquery',
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Tether: "tether",
+      Popper: "popper.js",
+      "window.Tether": "tether",
+      Alert: "exports-loader?Alert!bootstrap/js/dist/alert",
+      Button: "exports-loader?Button!bootstrap/js/dist/button",
+      Carousel: "exports-loader?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports-loader?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports-loader?Modal!bootstrap/js/dist/modal",
+      Popover: "exports-loader?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports-loader?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports-loader?Tab!bootstrap/js/dist/tab",
+      Tooltip: "exports-loader?Tooltip!bootstrap/js/dist/tooltip",
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
     }),
   ],
   module: {
